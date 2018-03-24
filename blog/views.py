0315f1d75a1e4916bc30db6122a009efe7ddcf96
Ian Evans <ievans3024@gmail.com>
2018-03-24 15:57:53 -0500

    newest posts first

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all().filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
